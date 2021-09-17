9014d66b0f210a622d995e2b8ba4d3bf2ab5aa31
psuryawa <pankaj.suryawanshi@here.com>
2021-09-17 18:42:27 +0530

    ResponsiveText comp added

---
+++ src/elements/ResponsiveText/ResponsiveText.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import style from './ResponsiveText.module.scss';
+
+
+const ResponsiveText = ({ text}) => {
+    
+    return <></>;
+};
+
+export default ResponsiveText;
